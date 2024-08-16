02038c6923b53c4403ec6b5d2e7627ee6fce773a
Manil Shah <manil.shah.2024@gmail.com>
2024-08-15 17:36:16 -0700

    Task Management Completed with the functionality (Checkpoint 1)

--- TaskManagementSystem.Core/obj/Debug/net8.0/TaskManagementSystem.Core.AssemblyInfo.cs
+++ TaskManagementSystem.Core/obj/Debug/net8.0/TaskManagementSystem.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b78e9f315e45dac0282b0e3297594ce9cc4072")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
